3b0cff4634d23c4e7236a97c7d47e3d5fc827c93
Nikolai Kochetov <nik-kochetov@yandex-team.ru>
2019-10-01 21:30:23 +0300

    Update TreeExecutor.

--- dbms/src/Processors/Executors/TreeExecutor.cpp
+++ dbms/src/Processors/Executors/TreeExecutor.cpp
@@ -49,6 +49,8 @@ static void validateTree(const Processors & processors, IProcessor * root)
             throw Exception("Processor with name " + node->getName() + " was visited twice while traverse in TreeExecutor. "
                             "Passed processors are not tree.", ErrorCodes::LOGICAL_ERROR);
 
+        is_visited[position] = true;
+
         checkProcessorHasSingleOutput(node);
 
         auto & children = node->getInputs();
